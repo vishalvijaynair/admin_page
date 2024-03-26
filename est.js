6b9b8c53fe6d5071111c467709d9dfc77dc58ea4
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-03-26 11:21:38 +0530

    Update est.js

--- est.js
+++ est.js
@@ -1,13 +1,1 @@
-const docRef = db.collection("service").doc("service center F");
-
-docRef.get().then((doc) => {
-  if (doc.exists) {
-    const data = doc.data();
-    // Use the data to update your HTML page
-    document.getElementById("user-table").textContent = data.someField;
-  } else {
-    console.log("No such document!");
-  }
-}).catch((error) => {
-  console.log("Error getting document:", error);
-});
+https://coding-test.turbolab.in/login/NDM1/c4hem0-45bb250c481dadf002af7d1dda868317
